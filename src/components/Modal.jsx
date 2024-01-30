505ff4f288ec3ecede366b1ffcb29db859b133ef
chandra11148 <cshekhar11148@gmail.com>
2024-01-30 16:51:33 +0530

    first commit

---
+++ src/components/Modal.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import ShowModal from "./ShowModal"; 
+
+function Modal() {
+    const [showModal,setShowModal] = useState(false);
+    const toggleModal = ()=>{
+        setShowModal(!showModal);
+    }
+  return (
+    <>
+        <div className='modal-main'>
+            <h1>User Details Modal</h1>
+            <button onClick={toggleModal} className='modal-btn'>Open Form</button>
+        
+        </div>
+        {showModal && <ShowModal handleToggle={toggleModal}/>}
+        
+    </>
+  )
+}
+
+export default Modal
---
+++ src/components/ShowModal.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+function ShowModal({ handleToggle }) {
+    const [userDetails,setUserDetails] = useState({username:"",email:"",phone:"",dob:""});
+    const validateInput=()=>{
+        
+        if(userDetails.phone.length!=10){
+            alert("Invalid phone number. Please enter a 10-digit phone number.");
+            return false;
+        }
+        const dobTime  = new Date(userDetails.dob).getTime();
+        const currTime = new Date().getTime();
+        if(dobTime>currTime){
+            alert("Invalid date of birth. Date of birth cannot be in the future.");
+            return false;
+        }
+        return true;
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        if(!validateInput()){
+            return;
+        }else{
+            handleToggle();
+        }
+    }
+    const handleInputChange=(e)=>{
+        setUserDetails((prevDetails)=>{return {...prevDetails,[e.target.name]:e.target.value}});
+    }
+  return (
+    <>
+   
+      <div className="modal-overlay" onClick={handleToggle}></div>
+      <div className="modal">
+      <div className="modal-content">
+        <h2>Fill Details</h2><br></br>
+        <form onSubmit={handleSubmit}>
+          <div className="formEl">
+            <label htmlFor="username">Username:</label>
+            <input required id="username" type="text" name="username" value={userDetails.username} onChange={handleInputChange}/>
+          </div>
+          <div className="formEl">
+            <label htmlFor="email">Email Address:</label>
+            <input required id="email" type="email" name="email" value={userDetails.email} onChange={handleInputChange}/>
+          </div>
+          <div className="formEl">
+            <label htmlFor="phone">Phone Number:</label>
+            <input required id="phone" pattern="[0-9]*" type="tel" name="phone" value={userDetails.phone} onChange={handleInputChange}/>
+          </div>
+          <div className="formEl">
+            <label htmlFor="dob">Date of Birth:</label>
+            <input required id="dob" type="date" name="dob" value={userDetails.dob} onChange={handleInputChange}/>
+          </div>
+          <button type="submit" className="modal-btn">Submit</button>
+        </form>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default ShowModal;
